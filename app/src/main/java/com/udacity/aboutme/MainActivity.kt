78f3431a45d8a50c01d3e46655c3431d6b99d1f5
Albina <alblupynos@gmail.com>
2020-02-28 15:35:51 +0200

    Implement About Me using View Binding.

---
+++ app/src/main/java/com/udacity/aboutme/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.udacity.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import com.udacity.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.doneButton.setOnClickListener{addNickName(it)}
+    }
+
+    private fun addNickName(view: View) {
+        val editText = binding.nicknameEdit
+        val nicknameTextView = binding.nicknameText
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+}
